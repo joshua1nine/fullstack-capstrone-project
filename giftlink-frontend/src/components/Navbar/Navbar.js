fab4c0af81659dc3c8247c7cc839ee58d9a96c71
Joshua Michael <joshua.kale.michael@gmail.com>
2024-06-10 16:32:45 -0500

    Add search page

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -20,6 +20,11 @@ export default function Navbar() {
               Gifts
             </a>
           </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/app/search">
+              Search
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
