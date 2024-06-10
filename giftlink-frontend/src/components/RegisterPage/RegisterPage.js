3b84cce9f2717aac2697cfabbe9e895a32bcf7a8
Joshua Michael <joshua.kale.michael@gmail.com>
2024-06-10 17:35:23 -0500

    Add login logic and fix register page

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -7,10 +7,10 @@ import "./RegisterPage.css";
 
 function RegisterPage() {
   //insert code here to create useState hook variables for firstName, lastName, email, password
-  [firstName, setFirstName] = useState("");
-  [lastName, setLastName] = useState("");
-  [email, setEmail] = useState("");
-  [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [showerr, setShowerr] = useState("");
 
@@ -62,16 +62,58 @@ function RegisterPage() {
             {/* insert code here to create input elements for all the variables - firstName, lastName, email, password */}
             <div className="mb-4">
               <label htmlFor="firstName" className="form label">
-                FirstName
+                First Name
               </label>
               <br />
               <input
                 id="firstName"
                 type="text"
                 className="form-control"
-                placeholder="Enter your firstName"
+                placeholder="Enter your first name"
                 value={firstName}
                 onChange={(e) => setFirstName(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="firstName" className="form label">
+                Last Name
+              </label>
+              <br />
+              <input
+                id="lastName"
+                type="text"
+                className="form-control"
+                placeholder="Enter your last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="email" className="form label">
+                Email
+              </label>
+              <br />
+              <input
+                id="email"
+                type="email"
+                className="form-control"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="form label">
+                Password
+              </label>
+              <br />
+              <input
+                id="email"
+                type="password"
+                className="form-control"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
 
